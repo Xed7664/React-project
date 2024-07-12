0024ea99970f3cedb4b76dbd45f08aa8f3c23d19
xed7664 <eduaneortega@gmail.com>
2024-07-12 17:58:32 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Edwin L. Ortega Jr.</h1>
+      <h2>Jei Navarro</h2>
+    </div>
+  )
+}
+
+export default App
